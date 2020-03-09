9bad6dfa9aad440ad2bccd0ca4d0eec247561b8e
JoaoPauloRomualdo <joaopaulohp94@hotmail.com>
2020-03-09 17:36:54 -0300

    Alteração do layout

--- js/main.js
+++ js/main.js
@@ -6,7 +6,53 @@ function limpar() {
 
 function voltar() {
 
-    setTimeout(function () { window.location.href = "index.html"; }, 3000);
+    setTimeout(function () { window.location.href = "index.html"; }, 2000);
 
 }
+
+//FUNÇÃO LER MAIS => ABA CURIOSIDADE
+function leiaMais(){
+    let pontos = document.getElementById("pontos");
+    let maisTexto = document.getElementById("mais");
+    let btnLeiaMais = document.getElementById("btnLeiaMais");
+
+    if(pontos.style.display ==="none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="Leia Mais";  
+    }
+    else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Leia Menos"; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
